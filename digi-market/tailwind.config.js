da964b9073d9d6800c573f79a5f6395dbd7c6236
nyk-o-brown <odoyobrown3@gmail.com>
2025-03-23 05:49:03 +0300

    msg : set react+vite+tailwind next : router-dom and components

---
+++ digi-market/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html,css}',
+    './public/index.html'
+  ],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
